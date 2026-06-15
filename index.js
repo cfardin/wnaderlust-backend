2c91fcbd238e18163b9b8c00a17cdb9dd1d471e8
cfardin <cfardin51@gmail.com>
2026-06-15 12:30:31 +0600

    added delete and update api

--- index.js
+++ index.js
@@ -1,7 +1,96 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
+const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
+dotenv.config();
+
+
+app.use(cors());
+app.use(express.json());
 
 const PORT = 5000;
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection("destinations");
+
+
+    // api to get all destinations 
+    app.get('/destination', async(req, res) =>{
+      const result = await destinationCollection.find().toArray();
+
+      res.json(result);
+    });
+
+    app.get('/destination/:id', async(req, res) =>{
+      const {id} = req.params;
+      const result = await destinationCollection.findOne(
+        {_id : new ObjectId(id)}
+      )
+      res.json(result);
+    });
+
+    // api for adding destinations in the database
+    app.post('/destination', async(req, res) =>{
+      const destination = req.body;
+      const result = await destinationCollection.insertOne(destination);
+
+      res.json(result);
+    });
+
+
+    // for updating destination data
+    app.patch('/destination/:id', async(req, res) =>{
+      const {id} = await req.params;
+      const updatedData = req.body;
+
+      const result = await destinationCollection.updateOne(
+        {_id : new ObjectId(id)},
+        {$set : updatedData}
+      )
+
+      res.json(result);
+    });
+
+
+    // for deleting destinations 
+    app.delete('/destination/:id', async (req, res) =>{
+      const {id} = await req.params;
+      const result = await destinationCollection.deleteOne({
+        _id : new ObjectId(id)
+      });
+
+      res.json(result);
+    })
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Server is running find, fuck u");
